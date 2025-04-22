776e38b2f0000a24e86640ab168ece67b4a38782
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-22 15:57:03 +0200

    [UPD] created controller

--- router/posts.js
+++ router/posts.js
@@ -4,34 +4,40 @@ const express = require("express");
 //importo la classe router
 const router = express.Router();
 
+//importo il ricettario
+const ricettario = ("../data/ricettario.js")
+
+//importo il controller delle ricette
+const 
+
 //index
 router.get("/", (req,res) => {
-    res.send("lista dei post");
+    res.send("lista delle ricette");
 })
 
 //show
 router.get("/:id",(req,res) => {
-    res.send("dettaglio del post"+req.params.id);
+    res.send("dettaglio della ricetta"+req.params.id);
 })
 
 //store
 router.post("/",(req,res) => {
-    res.send("inserimento nuovo post");
+    res.send("inserimento nuova ricetta");
 })
 
 //update
 router.put("/:id",(req,res) => {
-    res.send(`modifica totale del post ${req.params.id}`);
+    res.send(`modifica totale della ricetta ${req.params.id}`);
 })
 
 //modify
 router.patch("/:id",(req,res) => {
-    res.send(`modifica parziale del post ${req.params.id}`);
+    res.send(`modifica parziale della ricetta ${req.params.id}`);
 })
 
 //destroy
 router.delete("/:id",(req,res) => {
-    res.send(`cancellazione del post ${req.params.id}`);
+    res.send(`cancellazione della ricetta ${req.params.id}`);
 })
 
 //esporto il router
